f4ecfdd66c02db83f1084255f98e263d91d8275e
JeremyM45 <jeremyvs1945@gmail.com>
2022-05-06 12:04:18 -0700

    add ability for user to order both bread and pastry

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,34 +1,76 @@
 using System;
 using Bakery;
 
-class Prgram
+class Program
 {
+  static Pastry PastryOrder()
+  {
+      Console.WriteLine("\nHow many pastries do you want? \n<$" + Pastry.PricePerPastry + " per pastry>");
+      int PastryAmount = int.Parse(Console.ReadLine());
+      Pastry pastryOrder = new Pastry(PastryAmount);
+      return pastryOrder;
+  }
+  static Bread BreadOrder()
+  {
+    Console.WriteLine("\nHow many loafs of bread do you want? \n<$" + Bread.PricePerLoaf + " per loaf>");
+    int breadAmount = int.Parse(Console.ReadLine());
+    Bread breadOrder = new Bread(breadAmount);
+    return breadOrder;
+  }
+  static void DisplayTotal(Bread breadOrder, Pastry pastryOrder)
+  {
+    int totalPrice = breadOrder.TotalCost + pastryOrder.TotalCost;
+    if (breadOrder.Amount > 0 && pastryOrder.Amount == 0)
+    {
+      Console.WriteLine("\nYour order is " + breadOrder.Amount + " loaf(s) of bread\nTotal: $" + totalPrice);
+    }
+    else if (breadOrder.Amount == 0 && pastryOrder.Amount > 0)
+    {
+      Console.WriteLine("\nYour order is " + pastryOrder.Amount + " pastry(ies)\nTotal: $" + totalPrice);
+    }
+    else if (breadOrder.Amount > 0 && pastryOrder.Amount > 0)
+    {
+      Console.WriteLine("\nYour order is " + pastryOrder.Amount + " pastry(ies) and " + breadOrder.Amount + " loaf(s) of bread\nTotal: $" + totalPrice);
+    }
+    
+  }
   static void Main()
   {
     Console.WriteLine("Welcome To Pierre's Bakery");
     Console.WriteLine("----------------------------------------\nToday's Specials:\nBUY 2 OR MORE LOAFS OF BREAD AND GET EVERY THIRD FREE!\nBUY 2 OR MORE PASTRIES AND GET EVERY THIRD HALF OFF!\n----------------------------------------");
     Console.WriteLine("Would You like to order bread loafs or pastries?");
     string bakedGoodsChoice = Console.ReadLine();
+    Bread breadOrder = new Bread(0);
+    Pastry pastryOrder = new Pastry(0);
     if (bakedGoodsChoice.ToLower() == "bread" || bakedGoodsChoice.ToLower() == "bread loafs")
     {
-      Console.WriteLine("\nHow many loafs of bread do you want? \n<$" + Bread.PricePerLoaf + " per loaf>");
-      int breadAmount = int.Parse(Console.ReadLine());
-      Bread breadOrder = new Bread(breadAmount);
+      breadOrder = BreadOrder();
       breadOrder.BreadCost();
-      Console.WriteLine("\nYour order is " + breadOrder.Amount + " loafs of bread\nTotal: $" + breadOrder.TotalCost);
+      Console.WriteLine("Would you like to add pastries to your order? Y/N");
+      string orderMore = Console.ReadLine();
+      if(orderMore.ToLower() == "y")
+      {
+        pastryOrder = PastryOrder();
+        pastryOrder.PastryCost();
+      }
     }
     else if (bakedGoodsChoice.ToLower() == "pastry" || bakedGoodsChoice.ToLower() == "pastries")
     {
-      Console.WriteLine("\nHow many pastries do you want? \n<$" + Pastry.PricePerPastry + " per pastry>");
-      int PastryAmount = int.Parse(Console.ReadLine());
-      Pastry PastryOrder = new Pastry(PastryAmount);
-      PastryOrder.PastryCost();
-      Console.WriteLine("\nYour order is " + PastryOrder.Amount + " pastry(ies)\nTotal: $" + PastryOrder.TotalCost);
+      pastryOrder = PastryOrder();
+      pastryOrder.PastryCost();
+      Console.WriteLine("Would you like to add bread to your order? Y/N");
+      string orderMore = Console.ReadLine();
+      if(orderMore.ToLower() == "y")
+      {
+        breadOrder = BreadOrder();
+        breadOrder.BreadCost();
+      }
     }
     else
     {
       Console.WriteLine("\nInvalid Entry " + bakedGoodsChoice + " Is not an option, please try again\n");
       Main();
     }
+    DisplayTotal(breadOrder, pastryOrder);
   }
 }
