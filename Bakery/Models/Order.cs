dafe3140a170c8d641c0f93124734a14c0624ccf
JeremyM45 <jeremyvs1945@gmail.com>
2022-05-06 13:55:49 -0700

    add Order constructor

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -5,5 +5,12 @@ namespace Bakery
     public Bread BreadOrder {get; set;}
     public Pastry PastryOrder {get; set;}
     public int OrderTotal {get; set;}
+
+    public Order()
+    {
+      BreadOrder = new Bread(0);
+      PastryOrder = new Pastry(0);
+      OrderTotal = 0;
+    }
   }
 }
