8f68f0a74ca6c02cc81801029d0386b135de1b24
JeremyM45 <jeremyvs1945@gmail.com>
2022-05-06 14:07:42 -0700

    add TotalCostOfOrder method to Order

--- Bakery.Tests/ModelTests/Order.Test.cs
+++ Bakery.Tests/ModelTests/Order.Test.cs
@@ -13,30 +13,36 @@ namespace Bakery.Tests
       Order newOrder = new Order();
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
-    // [TestMethod]
-    // public void OrderTotal_ReturnsTheOrderTotalMessageFor5BreadLoafsAnd0Pastries_String()
-    // {
-    //   Order newOrder = newOrder();
-    //   newOrder.BreadOrder = new Bread(5);
-    //   newOrder.PastryOrder = new Pastry(0);
-    //   Assert.AreEqual("Your order is 5 loaf(s) of bread\nTotal: $20", newOrder.OrderTotal());
-    // }
-    // [TestMethod]
-    // public void OrderTotal_ReturnsTheOrderTotalMessageFor0BreadLoafsAnd5Pastries_String()
-    // {
-    //   Order newOrder = newOrder();
-    //   newOrder.BreadOrder = new Bread(0);
-    //   newOrder.PastryOrder = new Pastry(5);
-    //   Assert.AreEqual("Your order is 5 pastry(ies)\nTotal: $9", newOrder.OrderTotal());
-    // }
-    // [TestMethod]
-    // public void OrderTotal_ReturnsTheOrderTotalMessageFor5BreadLoafsAnd5Pastries_String()
-    // {
-    //   Order newOrder = newOrder();
-    //   newOrder.BreadOrder = new Bread(5);
-    //   newOrder.PastryOrder = new Pastry(5);
-    //   Assert.AreEqual("Your order is 5 pastry(ies) and 5 loaf(s) of bread\nTotal: $29", newOrder.OrderTotal());
-    // }
+    [TestMethod]
+    public void TotalCostOfOrder_ReturnsTheOrderTotalMessageFor5BreadLoafsAnd0Pastries_String()
+    {
+      Order newOrder = new Order();
+      newOrder.BreadOrder = new Bread(5);
+      newOrder.BreadOrder.BreadCost();
+      newOrder.PastryOrder = new Pastry(0);
+      newOrder.PastryOrder.PastryCost();
+      Assert.AreEqual("\nYour order is 5 loaf(s) of bread\nTotal: $20", newOrder.TotalCostOfOrder());
+    }
+    [TestMethod]
+    public void TotalCostOfOrder_ReturnsTheOrderTotalMessageFor0BreadLoafsAnd5Pastries_String()
+    {
+      Order newOrder = new Order();
+      newOrder.BreadOrder = new Bread(0);
+      newOrder.BreadOrder.BreadCost();
+      newOrder.PastryOrder = new Pastry(5);
+      newOrder.PastryOrder.PastryCost();
+      Assert.AreEqual("\nYour order is 5 pastry(ies)\nTotal: $9", newOrder.TotalCostOfOrder());
+    }
+    [TestMethod]
+    public void TotalCostOfOrder_ReturnsTheOrderTotalMessageFor5BreadLoafsAnd5Pastries_String()
+    {
+      Order newOrder = new Order();
+      newOrder.BreadOrder = new Bread(5);
+      newOrder.BreadOrder.BreadCost();
+      newOrder.PastryOrder = new Pastry(5);
+      newOrder.PastryOrder.PastryCost();
+      Assert.AreEqual("\nYour order is 5 pastry(ies) and 5 loaf(s) of bread\nTotal: $29", newOrder.TotalCostOfOrder());
+    }
     
   }
 }
--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -12,5 +12,25 @@ namespace Bakery
       PastryOrder = new Pastry(0);
       OrderTotal = 0;
     }
+    public string TotalCostOfOrder()
+    {
+      OrderTotal = BreadOrder.TotalCost + PastryOrder.TotalCost;
+      if (BreadOrder.Amount > 0 && PastryOrder.Amount == 0)
+      {
+        return "\nYour order is " + BreadOrder.Amount + " loaf(s) of bread\nTotal: $" + OrderTotal;
+      }
+      else if (BreadOrder.Amount == 0 && PastryOrder.Amount > 0)
+      {
+        return "\nYour order is " + PastryOrder.Amount + " pastry(ies)\nTotal: $" + OrderTotal;
+      }
+      else if (BreadOrder.Amount > 0 && PastryOrder.Amount > 0)
+      {
+        return "\nYour order is " + PastryOrder.Amount + " pastry(ies) and " + BreadOrder.Amount + " loaf(s) of bread\nTotal: $" + OrderTotal;
+      }
+      else
+      {
+        return "An Error Occured. " + BreadOrder.Amount + " or " + PastryOrder.Amount + " may be invalid numbers";
+      }
+    }
   }
 }
